1b43f5fb371106ea094f080f9edc8ef316eb1d31
Angelina <angelinauvarova943@gmail.com>
2026-04-02 12:52:35 +0500

    Задание 9: 'Шаблонизируй то'

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,23 +1,37 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createFilterTemplate() {
+function createFilterTemplate(filters) {
   return `
     <form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
+      ${filters.map(({ name, isDisabled, isChecked }) => `
+        <div class="trip-filters__filter">
+          <input
+            id="filter-${name}"
+            class="trip-filters__filter-input visually-hidden"
+            type="radio"
+            name="trip-filter"
+            ${isChecked ? 'checked' : ''}
+            ${isDisabled ? 'disabled' : ''}
+          >
+          <label class="trip-filters__filter-label" for="filter-${name}">
+            ${name}
+          </label>
+        </div>
+      `).join('')}
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>
   `;
 }
 
 export default class FilterView extends AbstractView {
+  #filters;
+
+  constructor(filters) {
+    super();
+    this.#filters = filters;
+  }
+
   get template() {
-    return createFilterTemplate();
+    return createFilterTemplate(this.#filters);
   }
 }
